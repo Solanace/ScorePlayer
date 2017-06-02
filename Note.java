403c002b9c5b394d955e648e9ed525499a56d4b2
Penn Wu <penn.z.wu@gmail.com>
2017-06-02 14:47:13 -0400

    Added in scores to test on and messed around with ScorePlayer.pde

--- Note.java
+++ Note.java
@@ -2,9 +2,13 @@ class Note {
     double freq, beatStart, beatEnd;
     boolean isRest;
     
-    public Note(double frequency, double start, double end) {
-        freq=frequency;
-        beatStart=start;
-        beatEnd=end;
+    Note(double frequency, double start, double end) {
+        freq = frequency;
+        beatStart = start;
+        beatEnd = end;
+    }
+    
+    public String toString() {
+      return freq + ", " + beatStart + ", " + beatEnd;
     }
 }
---
+++ ScorePlayer.pde
@@ -0,0 +1,20 @@
+Note n;
+PImage score;
+
+public void setup() {
+  size(600, 600);
+  score = loadImage("twinkle0.gif");
+  score.updatePixels();
+}
+
+public void draw() {
+  image(score, 0, 0);
+  delay(1000);
+  score.loadPixels();
+  for (int i = 0; i < score.width * score.height; i += 4) {
+    score.pixels[i] = color(0, 0, 0);
+  }
+  score.updatePixels();
+  delay(1000);
+  score = loadImage("twinkle1.gif");
+}
